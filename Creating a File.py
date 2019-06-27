2d871b48709701f55180f1aed6dbae1fad3a0fe5
RiskyTrick <38748374+RiskyTrick@users.noreply.github.com>
2019-06-27 09:48:08 +0530

    Add files via upload

---
+++ Creating a File.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+def createfile(Risky):
+    f=open(Risky,"w")
+    for i in range(10):
+        f.write("This is %d"%i)
+    print("The File Has been Created Susccessfully")
+    f.close()
+    return
+createfile("Risky.txt")
+
+
+# In[4]:
+
+
+open(Risky,"r")
+
